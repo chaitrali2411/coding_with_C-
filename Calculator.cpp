13b0642495fbbfd1353fac25370756b570e23ed8
Chaitrali Kadam <chaitralibalukadam@gmail.com>
2025-04-07 23:41:52 -0600

    Calculator code

---
+++ Calculator.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a, b ;
+   cout << "Enter the a value : ";
+   cin >> a;
+   cout << "Enter the b value : ";
+   cin >> b;
+
+   cout << "The addition of two values " << (a+b)<<endl;
+   cout << "The subtraction of two values " << (a-b)<<endl;
+   cout << "The multiplication of two values " << (a*b)<<endl;
+   cout << "The division of two values " << (a/b)<<endl;
+    return 0;
+}
